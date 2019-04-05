ebd9b951c5a856023cdbe5cbb83700f184d69d12
olga <miss-meeseek.noreply.github.com>
2019-04-05 21:56:00 +0200

    string comparision repaired

--- app/src/main/java/com/example/hangm/MainActivity.kt
+++ app/src/main/java/com/example/hangm/MainActivity.kt
@@ -21,13 +21,12 @@ class MainActivity : AppCompatActivity() {
     private var secret: String = ""
     var mask:String = ""
     var winner: Int = 0
-    private var lifes:Int = 0
+    private var lives:Int = 0
+    private var livesMax:Int = 7
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val array = resources.getStringArray(R.array.dictionary)
 
         imageView = findViewById<ImageView>(R.id.imageView)
         imageView.setImageResource(R.drawable.hangman0)
@@ -37,11 +36,15 @@ class MainActivity : AppCompatActivity() {
         editText = this.findViewById(R.id.editText)
         button = this.findViewById(R.id.button)
 
+        //Initiate dictionary
+        val array = resources.getStringArray(R.array.dictionary)
+        //Choose word from dictionary
         val r = Random()
         val randomNumber = r.nextInt(array.size)
         secret = array[randomNumber].toString()
         len = secret.length
 
+        //Create word encrypted with "????"
         var i = 0
         while(i < len) {
             mask += "?"
@@ -50,49 +53,54 @@ class MainActivity : AppCompatActivity() {
         word.text = mask
         Toast.makeText(applicationContext, "hint: " + array[randomNumber].toString(), Toast.LENGTH_SHORT).show()
 
+        //Listener for button "TRY"
         button.setOnClickListener {
             val input = editText.text
             editText.hideKeyboard()
 
             if (input.isNotEmpty()) {
+                //Check if input was correct
                 checkInput(input)
             } else {
+                //Show Toast
                 tryMore()
             }
         }
     }
 
+    //Function check if choosed word contains sign from input
     private fun checkInput(input: Editable) {
-        if(input.length != 1) {
+        if(input.length != 1) { //Input is incorrect if it contains more or less than one sign
             tryMore()
         }
         else {
-            Toast.makeText(applicationContext, "Inputed Sign is $input", Toast.LENGTH_SHORT).show()
             var i = 0
-            var j = 0
+            var contains = 0
             while(i < len) {
                 if(secret[i].toString() == input.toString()) {
-                    var head = mask.dropLast(len - i)
-                    var tail = mask.drop(i + 1)
-                    mask = "$head$input$tail"
+                    val head = mask.dropLast(len - i)
+                    val tail = mask.drop(i + 1)
+                    mask = head + input + tail
                     winner++
-                    j++
+                    contains++
                 }
                 i++
             }
             word.text = mask
-            if(j == 0) {
-                lifes++
-                var imageName = "hangman"+lifes
+            if(contains == 0) {
+                lives++
+                val imageName = "hangman$lives"
                 val id = resources.getIdentifier(imageName, "drawable", packageName)
                 imageView.setImageResource(id)
             }
+            Toast.makeText(applicationContext, "Inputted Sign is $input", Toast.LENGTH_SHORT).show()
         }
         editText.setText("")
         checkWinner()
     }
 
 
+    //Function shows Toast for user if input was empty or incorrect
     private fun tryMore() {
         Toast.makeText(applicationContext, "Try one more time!", Toast.LENGTH_SHORT).show()
     }
@@ -104,7 +112,7 @@ class MainActivity : AppCompatActivity() {
             editText.isEnabled = false;
             text.text = "You win :--) !"
         }
-        if(lifes == 7) {
+        if(lives == livesMax) {
             button.isEnabled = false;
             text.text = "You lose :'( !"
             word.text = secret
@@ -112,6 +120,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Function hides the keyboard 
     @SuppressLint("ServiceCast")
     fun View.hideKeyboard() {
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
